e6508e21c24324c64584bbbb810cb25da771fea7
Todd Mohney <toddmohney@gmail.com>
2015-09-27 13:48:00 -0400

    Add db and connection string parser

---
+++ product-executable/Config.hs
@@ -0,0 +1,50 @@
+module Config 
+  ( dbConnectionString 
+  ) where
+
+  import Control.Applicative ((<$>))
+  import Data.ByteString.Char8 (ByteString, pack)
+  import Data.List (intersperse)
+  import System.Environment (getEnv, lookupEnv)
+
+  data DbConnectionString = DbConnectionString {
+      dbname   :: String,
+      user     :: Maybe String,
+      password :: Maybe String,
+      host     :: Maybe String,
+      port     :: Maybe Int
+    } deriving (Show)
+
+  toStr :: DbConnectionString -> String
+  toStr cStr = concat . (intersperse " ") $ [
+    ("dbname=" ++ (dbname cStr))
+    , (maybe "" (\x -> "user=" ++ x) (user cStr))
+    , (maybe "" (\x -> "password=" ++ x) (password cStr))
+    , (maybe "host=localhost" (\x -> "host=" ++ x) (host cStr))
+    , (maybe "port=5432" (\x -> "port=" ++ show x) (port cStr))
+    ]
+
+  dbConnectionString :: IO ByteString
+  dbConnectionString = (pack . toStr) <$> readDbConnectionString
+
+  readDbConnectionString :: IO DbConnectionString
+  readDbConnectionString = do
+    _dbname   <- getEnv "DB_NAME"
+    _user     <- lookupEnv "DB_USER"
+    _password <- lookupEnv "DB_PASS"
+    _host     <- lookupEnv "DB_HOST"
+    _port     <- lookupEnv "DB_PORT"
+
+    return DbConnectionString {
+      dbname   = _dbname,
+      user     = _user,
+      password = _password,
+      host     = _host,
+      port     = parsePort _port
+    }
+
+  parsePort :: Maybe String -> Maybe Int
+  parsePort Nothing = Nothing
+  parsePort (Just p) = Just (read p)
+
+
--- product-executable/Main.hs
+++ product-executable/Main.hs
@@ -1,10 +1,28 @@
-module Main where
-  import qualified CLI.Base as CLI
-  import System.Environment              (getArgs)
+{-# LANGUAGE OverloadedStrings          #-}
 
-  main :: IO ()
-  main = do
+module Main where
+  import qualified Config
+  import Control.Monad.IO.Class  (liftIO)
+  import Control.Monad.Logger    (runStderrLoggingT)
+  import Database.Persist.Postgresql
+  import Products.Model
+  import System.Environment (getArgs)
+  import qualified CLI.Base as CLI
+
+  runApplication :: IO ()
+  runApplication = do
     args <- getArgs
     case (CLI.parseCommand args) of
       Nothing    -> CLI.showUsage
       (Just cmd) -> CLI.execCommand cmd
+
+  initDB :: IO ()
+  initDB = do
+    connStr <- Config.dbConnectionString
+    runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+      flip runSqlPersistMPool pool $ do
+        runMigration migrateAll
+
+  main :: IO ()
+  main = initDB >> runApplication
+
