61453a035b01e17611c6413bed2771d43de87410
Todd Mohney <toddmohney@gmail.com>
2015-10-02 17:42:44 -0500

    Remove unused import

--- lib/Models.hs
+++ lib/Models.hs
@@ -9,7 +9,6 @@
 module Models where
   import Database.Persist.TH
   import Data.Text (Text)
-  import Data.Time
 
   share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
     Product json sql=products
