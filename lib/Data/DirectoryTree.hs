0e7371a67d04872d9d84f6a65607fcd0fdcd924b
Todd Mohney <toddmohney@gmail.com>
2015-10-17 12:39:41 -0400

    Extract DirectoryTree module

---
+++ lib/Data/DirectoryTree.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.DirectoryTree 
+  ( DirectoryTree
+  , addToDirectoryTree
+  , createNode
+  ) where
+
+  import Data.List (partition)
+  import qualified Data.Text as T
+  import Data.Tree (Tree(Node))
+
+  type DirectoryTree = Tree FilePath
+
+  createNode :: FilePath -> DirectoryTree
+  createNode file = Node file []
+
+  addToDirectoryTree :: DirectoryTree -> FilePath -> DirectoryTree
+  addToDirectoryTree featureTree filePath = addToDirectoryTree' featureTree (splitFileName $ T.pack filePath)
+    where
+      splitFileName :: T.Text -> [T.Text]
+      splitFileName = (T.splitOn "/") . throwOutLeadingSlash 
+
+      throwOutLeadingSlash :: T.Text -> T.Text
+      throwOutLeadingSlash txt
+        | (T.head txt) == '/' = T.tail txt
+        | otherwise           = txt
+
+  addToDirectoryTree' :: DirectoryTree -> [T.Text] -> DirectoryTree
+  addToDirectoryTree' featureTree []                       = featureTree
+  addToDirectoryTree' (Node label forest) [file]           = Node label $ forest ++ [(createNode $ T.unpack file)]
+  addToDirectoryTree' (Node label forest) (directory:rest) =
+    let (matches, nonMatches) = partition (matchesLabel directory) forest in
+        case matches of
+          [] -> do
+            let newNode = createNode $ T.unpack directory
+            Node label $ (addToDirectoryTree' newNode rest):forest
+          [node] -> do
+            Node label $ (addToDirectoryTree' node rest):nonMatches -- effectively replace the matching node
+          (_:_) -> error "There should be a maximum of 1 match in the forest"
+
+  matchesLabel :: T.Text -> DirectoryTree -> Bool
+  matchesLabel file (Node label _) = (T.unpack file) == label
+
--- lib/Features/Feature.hs
+++ lib/Features/Feature.hs
@@ -3,50 +3,25 @@
 module Features.Feature where
   import CommonCreatures (WithErr)
   import Control.Applicative ((<$>))
-  import Data.List (stripPrefix, partition)
+  import Data.DirectoryTree (DirectoryTree, createNode, addToDirectoryTree)
+  import Data.List (stripPrefix)
   import Data.Maybe (mapMaybe)
-  import Data.Text (Text, pack, unpack, splitOn)
-  import Data.Tree (Tree(Node))
   import Control.Monad.Except (throwError)
   import Control.Monad.IO.Class (liftIO)
-  import qualified Data.Tree as T
   import System.Exit (ExitCode(ExitFailure, ExitSuccess))
   import System.Process (readProcessWithExitCode)
 
   type Feature                = String
   type FeatureFile            = FilePath
-  type FeatureTree            = T.Tree Feature
 
-  getFeatures :: FilePath -> WithErr FeatureTree
-  getFeatures path = buildFeatureTree <$> findFeatureFiles path
+  getFeatures :: FilePath -> WithErr DirectoryTree
+  getFeatures path = buildDirectoryTree <$> findFeatureFiles path
 
-  buildFeatureTree :: [FeatureFile] -> FeatureTree
-  buildFeatureTree = foldr (\x acc -> addToFileTree acc (splitFilename x)) rootNode
+  buildDirectoryTree :: [FeatureFile] -> DirectoryTree
+  buildDirectoryTree = foldr (\featureFile dirTree -> addToDirectoryTree dirTree featureFile) rootNode
     where
-      splitFilename :: FeatureFile -> [Text]
-      splitFilename fn = splitOn "/" (pack fn)
-
-      rootNode :: Tree String
-      rootNode = Node "featuresRoot" []
-
-  addToFileTree :: FeatureTree -> [Text] -> FeatureTree
-  addToFileTree featureTree []                       = featureTree
-  addToFileTree (Node label forest) [file]           = Node label $ forest ++ [(createNode $ unpack file)]
-  addToFileTree (Node label forest) (directory:rest) =
-    let (matches, nonMatches) = partition (matchesLabel directory) forest in
-        case matches of
-          [] -> do
-            let newNode = createNode $ unpack directory
-            Node label $ (addToFileTree newNode rest):forest
-          [node] -> do
-            Node label $ (addToFileTree node rest):nonMatches -- effectively replace the matching node
-          (_:_) -> error "There should be a maximum of 1 match in the forest"
-
-  matchesLabel :: Text -> FeatureTree -> Bool
-  matchesLabel file (Node label _) = (unpack file) == label
-
-  createNode :: Feature -> FeatureTree
-  createNode file = Node file []
+      rootNode :: DirectoryTree
+      rootNode = createNode "featuresRoot"
 
   findFeatureFiles :: FilePath -> WithErr [FeatureFile]
   findFeatureFiles path = do
