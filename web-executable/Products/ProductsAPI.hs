52d93ef9e6e0213fd9ba8785982db80f05f1c99d
Todd Mohney <toddmohney@gmail.com>
2015-11-24 19:17:48 -0500

    [#44] Add endpoint for creating new Products

--- web-executable/Products/ProductsAPI.hs
+++ web-executable/Products/ProductsAPI.hs
@@ -11,9 +11,13 @@ module Products.ProductsAPI
   , productsServer
   ) where
 
+  import Control.Monad (mzero)
+  import Control.Monad.Except (runExceptT)
   import Control.Monad.IO.Class (liftIO)
+  import Control.Monad.Trans.Either (left)
   import Data.Aeson
   import qualified Data.Text               as T
+  import qualified Data.ByteString.Lazy.Char8 as BS
   import Models
   import qualified Products.DomainTermsAPI as DT
   import Products.FeaturesAPI
@@ -23,20 +27,18 @@ module Products.ProductsAPI
   import ServantUtilities (Handler)
   import qualified Products.UserRolesAPI   as UR
 
-  type ProductsAPI = "products" :> ProductsEndpoints
-                :<|> "products" :> (
-                                     ProductIDCapture :> FeaturesAPI
-                                :<|> ProductIDCapture :> FeatureAPI
-                                :<|> ProductIDCapture :> DT.DomainTermsAPI
-                                :<|> ProductIDCapture :> DT.CreateDomainTermsAPI
-                                :<|> ProductIDCapture :> UR.UserRolesAPI
-                                :<|> ProductIDCapture :> UR.CreateUserRolesAPI
-                                   )
+  type ProductsAPI = "products" :> Get '[JSON] [APIProduct]
+                :<|> "products" :> ReqBody '[JSON] APIProduct :> Post '[JSON] APIProduct
+                :<|> "products" :> ProductIDCapture :> FeaturesAPI
+                :<|> "products" :> ProductIDCapture :> FeatureAPI
+                :<|> "products" :> ProductIDCapture :> DT.DomainTermsAPI
+                :<|> "products" :> ProductIDCapture :> DT.CreateDomainTermsAPI
+                :<|> "products" :> ProductIDCapture :> UR.UserRolesAPI
+                :<|> "products" :> ProductIDCapture :> UR.CreateUserRolesAPI
 
-  type ProductsEndpoints = Get '[JSON] [APIProduct]
   type ProductIDCapture = Capture "id" P.ProductID
 
-  data APIProduct = APIProduct { productID :: P.ProductID
+  data APIProduct = APIProduct { productID :: Maybe P.ProductID
                                , name      :: T.Text
                                , repoUrl   :: T.Text
                                } deriving (Show)
@@ -48,8 +50,16 @@ module Products.ProductsAPI
              , "repoUrl" .= prodRepoUrl
              ]
 
+  instance FromJSON APIProduct where
+    parseJSON (Object v) = APIProduct <$>
+                          v .:? "id" <*>
+                          v .: "name" <*>
+                          v .: "repoUrl"
+    parseJSON _          = mzero
+
   productsServer :: Server ProductsAPI
   productsServer = products
+              :<|> createProduct
               :<|> productsFeatures
               :<|> productsFeature
               :<|> DT.productsDomainTerms
@@ -60,6 +70,22 @@ module Products.ProductsAPI
   productsAPI :: Proxy ProductsAPI
   productsAPI = Proxy
 
+  createProduct :: APIProduct -> Handler APIProduct
+  createProduct (APIProduct _ prodName prodRepoUrl) = do
+    let newProduct = P.Product prodName prodRepoUrl
+    prodID <- liftIO $ P.createProduct newProduct
+    result <- liftIO $ runExceptT (P.updateRepo newProduct prodID)
+    case result of
+      Left err ->
+        -- In the case where the repo cannot be retrieved,
+        -- It's probably a good idea to rollback the Product creation here.
+        left $ err503 { errBody = BS.pack err }
+      Right _ ->
+        return $ APIProduct { productID = Just prodID
+                            , name      = prodName
+                            , repoUrl   = prodRepoUrl
+                            }
+
   products :: Handler [APIProduct]
   products = do
     prods <- liftIO P.findProducts
@@ -68,7 +94,7 @@ module Products.ProductsAPI
         toProduct dbProduct = do
           let dbProd   = P.toProduct dbProduct
           let dbProdID = P.toProductID dbProduct
-          APIProduct { productID = dbProdID
+          APIProduct { productID = Just dbProdID
                      , name      = productName dbProd
                      , repoUrl   = productRepoUrl dbProd }
 
@@ -77,17 +103,20 @@ module Products.ProductsAPI
   instance SD.ToSample [APIProduct] [APIProduct] where
     toSample _ = Just $ [ sampleMonsterProduct, sampleCreatureProduct ]
 
+  instance SD.ToSample APIProduct APIProduct where
+    toSample _ = Just sampleCreatureProduct
+
   instance SD.ToCapture (Capture "id" P.ProductID) where
     toCapture _ = SD.DocCapture "id" "Product id"
 
   sampleMonsterProduct :: APIProduct
-  sampleMonsterProduct = APIProduct { productID = 1
+  sampleMonsterProduct = APIProduct { productID = Just 1
                                     , name      = "monsters"
                                     , repoUrl   = "http://monsters.com/repo.git"
                                     }
 
   sampleCreatureProduct :: APIProduct
-  sampleCreatureProduct = APIProduct { productID = 2
+  sampleCreatureProduct = APIProduct { productID = Just 2
                                      , name      = "creatures"
                                      , repoUrl   = "ssh://creatures.com/repo.git"
                                      }
