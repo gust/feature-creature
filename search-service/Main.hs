61f8c591a24e5c8f741de7fc8988b6570bf8d18b
Todd Mohney <toddmohney@gmail.com>
2015-12-06 19:24:28 -0500

    Refactoring toward unobtrusive configuration (2)
    Encapsulating config, then explictly passing config data
    Reference: https://slpopejoy.github.io/posts/Effectful02.html
    
    [gust/feature-creature#58]

--- search-service/Main.hs
+++ search-service/Main.hs
@@ -1,44 +1,48 @@
 module Main where
 
-import CommonCreatures (WithErr)
-import Config
-import Control.Monad.Except (runExceptT, throwError)
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Except (runExceptT)
 import Data.Text (pack)
 import Data.Traversable (sequence)
 import Features.Feature as F
-import Features.SearchableFeature as SF
+import qualified Features.SearchableFeature as SF
 import System.Environment (getEnv)
+
+data AppConfig =
+  AppConfig { elasticSearchUrl :: String
+            , featureFilePath  :: String
+            }
 
 main :: IO ()
 main = do
-  esUrl              <- getEnv "FC_ELASTIC_SEARCH_URL"
-  projectsBaseDir    <- getEnv "FC_SERVER_PROJECT_ROOT"
-  featureFiles       <- runExceptT $ F.findFeatureFiles "/Users/toddmohney/workspace/feature-creature/.app-data/products/39/repo"
+  esUrl         <- getEnv "FC_ELASTIC_SEARCH_URL"
+  dataFilesPath <- getEnv "FC_DATA_FILES_PATH"
+  indexFeatures (AppConfig esUrl (dataFilesPath ++ "/products/39/repo"))
 
-  let config = Config esUrl
+indexFeatures :: AppConfig -> IO ()
+indexFeatures appConfig = do
+  featureFiles <- runExceptT $ F.findFeatureFiles (featureFilePath appConfig)
   case featureFiles of
     Left errorStr ->
       putStrLn errorStr
     Right features -> do
-      searchableFeatures <- sequence $ buildSearchableFeatures projectsBaseDir features
+      searchableFeatures <- sequence $ buildSearchableFeatures appConfig features
       replies <- SF.indexFeatures searchableFeatures
       putStrLn $ foldr (\x acc -> acc ++ "\n" ++ (show x)) "" replies
 
-buildSearchableFeatures :: FilePath -> [FilePath] -> [IO SearchableFeature]
-buildSearchableFeatures basePath filePaths =
-  let fileDetails = map (getFileConetnts basePath) filePaths
+buildSearchableFeatures :: AppConfig -> [FilePath] -> [IO SF.SearchableFeature]
+buildSearchableFeatures appConfig filePaths =
+  let fileDetails = map (getFileConetnts appConfig) filePaths
   in
     map (fmap buildSearchableFeature) fileDetails
 
-getFileConetnts :: FilePath -> FilePath -> IO (FilePath, String)
-getFileConetnts basePath filePath = do
-  let fullFilePath = basePath ++ "/.app-data/products/39/repo" ++ filePath
+getFileConetnts :: AppConfig -> FilePath -> IO (FilePath, String)
+getFileConetnts appConfig filePath = do
+  let fullFilePath = (featureFilePath appConfig) ++ filePath
   fileContents <- readFile fullFilePath
   return (filePath, fileContents)
 
-buildSearchableFeature :: (FilePath, String) -> SearchableFeature
+buildSearchableFeature :: (FilePath, String) -> SF.SearchableFeature
 buildSearchableFeature (filePath, fileContents) =
-  SearchableFeature { featurePath = pack filePath
-                    , featureText = pack fileContents
-                    }
+  SF.SearchableFeature { SF.featurePath = pack filePath
+                       , SF.featureText = pack fileContents
+                       }
