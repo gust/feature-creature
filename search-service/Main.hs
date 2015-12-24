3b81b264df6856ccdfd9c88482f9b64fb9a0ab1f
Todd Mohney <toddmohney@gmail.com>
2015-12-24 11:56:24 -0500

    Refactor search-service Main
    [gust/feature-creature#58]

--- search-service/Main.hs
+++ search-service/Main.hs
@@ -21,33 +21,33 @@ main = do
 
 processJobs :: App ()
 processJobs = do
-  awsCfg   <- reader awsConfig
-  basePath <- reader featureFilePath
+  cfg    <- ask
+  awsCfg <- reader awsConfig
 
   liftIO $ forever $ do
-    let say = putStrLn
-
-    jobs <- getSQSMessages awsCfg
-    say $ "Got enqueued jobs: " ++ (show jobs)
-
-    forM_ jobs $ \job -> do
-      case job of
+    enqueuedJobs <- getSQSMessages awsCfg
+    forM_ enqueuedJobs $ \enqueuedJob -> do
+      case enqueuedJob of
         Left err ->
-          putStrLn err
-        Right j ->
-          case payload j of
-            CodeRepository repositoryPath -> do
-              let featureFileBasePath = basePath ++ (Text.unpack repositoryPath)
-              -- this could become useful as log output
-              putStrLn $ "Finding feature files at: " ++ featureFileBasePath
-
-              featureFiles <- runExceptT $ F.findFeatureFiles featureFileBasePath
-              case featureFiles of
-                Left errorStr ->
-                  putStrLn errorStr
-                Right features ->
-                  Indexer.indexFeatures $ map (\featurePath -> featureFileBasePath ++ featurePath) features
-            _ ->
-              say $ "Unprocessable job type: " ++ (Text.unpack $ jobType j)
-
+          liftIO $ putStrLn err
+        Right job ->
+          runReaderT (processJob job) cfg
     threadDelay 10000
+
+processJob :: Job CodeRepository -> App ()
+processJob job = do
+  basePath <- reader featureFilePath
+  let say = liftIO . putStrLn
+  case payload job of
+    CodeRepository repositoryPath -> do
+      let featureFileBasePath = basePath ++ (Text.unpack repositoryPath)
+      say $ "Finding feature files at: " ++ featureFileBasePath
+
+      featureFiles <- liftIO $ runExceptT $ F.findFeatureFiles featureFileBasePath
+      case featureFiles of
+        Left errorStr ->
+          liftIO $ putStrLn errorStr
+        Right features ->
+          liftIO $ Indexer.indexFeatures $ map (\featurePath -> featureFileBasePath ++ featurePath) features
+    _ ->
+      say $ "Unprocessable job type: " ++ (Text.unpack $ jobType job)
