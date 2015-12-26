f585fcbb4b6db7f8499b003f0eafa73916fecd74
Todd Mohney <toddmohney@gmail.com>
2015-12-26 13:24:14 -0500

    Modify DomainTermsAPI for ReaderT config compatibility
    [gust/feature-creature#60]

--- web-executable/Products/DomainTermsAPI.hs
+++ web-executable/Products/DomainTermsAPI.hs
@@ -6,79 +6,79 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Products.DomainTermsAPI where
-  import Control.Monad (mzero)
-  import Control.Monad.IO.Class (liftIO)
-  import Data.Aeson
-  import Data.Int (Int64)
-  import qualified Data.Text              as T
-  import qualified DomainTerms.DomainTerm as DT
-  import Models
-  import qualified Products.Product as P
-  import Servant
-  import qualified Servant.Docs     as SD
-  import ServantUtilities (Handler)
+import App (App (..))
+import Control.Monad (mzero)
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
+import Data.Int (Int64)
+import qualified Data.Text              as T
+import qualified DomainTerms.DomainTerm as DT
+import Models
+import qualified Products.Product as P
+import Servant
+import qualified Servant.Docs     as SD
 
-  type DomainTermsAPI       = "domain-terms" :> Get '[JSON] [APIDomainTerm]
-  type CreateDomainTermsAPI = "domain-terms" :> ReqBody '[JSON] APIDomainTerm :> Post '[JSON] APIDomainTerm
+type DomainTermsAPI       = "domain-terms" :> Get '[JSON] [APIDomainTerm]
+type CreateDomainTermsAPI = "domain-terms" :> ReqBody '[JSON] APIDomainTerm :> Post '[JSON] APIDomainTerm
 
-  data APIDomainTerm = APIDomainTerm { domainTermID :: Maybe Int64
-                                     , productID    :: Maybe ProductId
-                                     , title        :: T.Text
-                                     , description  :: T.Text
-                                     } deriving (Show)
+data APIDomainTerm = APIDomainTerm { domainTermID :: Maybe Int64
+                                   , productID    :: Maybe ProductId
+                                   , title        :: T.Text
+                                   , description  :: T.Text
+                                   } deriving (Show)
 
-  instance ToJSON APIDomainTerm where
-    toJSON (APIDomainTerm termID prodID termTitle termDescription) =
-      object [ "id"          .= termID
-             , "productID"   .= prodID
-             , "title"       .= termTitle
-             , "description" .= termDescription
-             ]
+instance ToJSON APIDomainTerm where
+  toJSON (APIDomainTerm termID prodID termTitle termDescription) =
+    object [ "id"          .= termID
+           , "productID"   .= prodID
+           , "title"       .= termTitle
+           , "description" .= termDescription
+           ]
 
-  instance FromJSON APIDomainTerm where
-    parseJSON (Object v) = APIDomainTerm <$>
-                          v .:? "id" <*>
-                          v .:? "productID" <*>
-                          v .: "title" <*>
-                          v .: "description"
-    parseJSON _          = mzero
+instance FromJSON APIDomainTerm where
+  parseJSON (Object v) = APIDomainTerm <$>
+                        v .:? "id" <*>
+                        v .:? "productID" <*>
+                        v .: "title" <*>
+                        v .: "description"
+  parseJSON _          = mzero
 
-  createDomainTerm :: P.ProductID -> APIDomainTerm -> Handler APIDomainTerm
-  createDomainTerm pID (APIDomainTerm _ _ t d) = do
-    termID <- liftIO $ DT.createDomainTerm (DT.DomainTerm (toKey pID) t d)
-    return $ APIDomainTerm { domainTermID = Just termID
-                           , productID    = Just (toKey pID)
-                           , title        = t
-                           , description  = d
-                           }
+createDomainTerm :: P.ProductID -> APIDomainTerm -> App APIDomainTerm
+createDomainTerm pID (APIDomainTerm _ _ t d) = do
+  termID <- liftIO $ DT.createDomainTerm (DT.DomainTerm (toKey pID) t d)
+  return $ APIDomainTerm { domainTermID = Just termID
+                         , productID    = Just (toKey pID)
+                         , title        = t
+                         , description  = d
+                         }
 
-  productsDomainTerms :: P.ProductID -> Handler [APIDomainTerm]
-  productsDomainTerms prodID = do
-    domainTerms <- liftIO $ DT.findByProductId $ toKey prodID
-    return $ map toDomainTerm domainTerms
-      where
-        toDomainTerm dbDomainTerm = do
-          let dbTerm   = DT.toDomainTerm dbDomainTerm
-          let dbTermID = DT.toDomainTermID dbDomainTerm
-          APIDomainTerm { domainTermID = Just dbTermID
-                        , productID    = Just $ domainTermProductId dbTerm
-                        , title        = domainTermTitle dbTerm
-                        , description  = domainTermDescription dbTerm
-                        }
+productsDomainTerms :: P.ProductID -> App [APIDomainTerm]
+productsDomainTerms prodID = do
+  domainTerms <- liftIO $ DT.findByProductId $ toKey prodID
+  return $ map toDomainTerm domainTerms
+    where
+      toDomainTerm dbDomainTerm = do
+        let dbTerm   = DT.toDomainTerm dbDomainTerm
+        let dbTermID = DT.toDomainTermID dbDomainTerm
+        APIDomainTerm { domainTermID = Just dbTermID
+                      , productID    = Just $ domainTermProductId dbTerm
+                      , title        = domainTermTitle dbTerm
+                      , description  = domainTermDescription dbTerm
+                      }
 
-  -- API Documentation Instance Definitions --
+-- API Documentation Instance Definitions --
 
-  instance SD.ToSample [APIDomainTerm] [APIDomainTerm] where
-    toSample _ = Just $ [ sampleAPIDomainTerm, sampleAPIDomainTerm2 ]
+instance SD.ToSample [APIDomainTerm] [APIDomainTerm] where
+  toSample _ = Just $ [ sampleAPIDomainTerm, sampleAPIDomainTerm2 ]
 
-  instance SD.ToSample APIDomainTerm APIDomainTerm where
-    toSample _ = Just $ samplePostBody
+instance SD.ToSample APIDomainTerm APIDomainTerm where
+  toSample _ = Just $ samplePostBody
 
-  sampleAPIDomainTerm :: APIDomainTerm
-  sampleAPIDomainTerm = APIDomainTerm (Just 1) (Just (toKey (10::Integer))) "mutation" "The genetic alteration granting monster powers"
+sampleAPIDomainTerm :: APIDomainTerm
+sampleAPIDomainTerm = APIDomainTerm (Just 1) (Just (toKey (10::Integer))) "mutation" "The genetic alteration granting monster powers"
 
-  sampleAPIDomainTerm2 :: APIDomainTerm
-  sampleAPIDomainTerm2 = APIDomainTerm (Just 2) (Just (toKey (10::Integer))) "vampirism" "The disease affecting Vampires"
+sampleAPIDomainTerm2 :: APIDomainTerm
+sampleAPIDomainTerm2 = APIDomainTerm (Just 2) (Just (toKey (10::Integer))) "vampirism" "The disease affecting Vampires"
 
-  samplePostBody :: APIDomainTerm
-  samplePostBody = APIDomainTerm (Just 2) (Just (toKey (10::Integer))) "monsterism" "The quality of being a monster"
+samplePostBody :: APIDomainTerm
+samplePostBody = APIDomainTerm (Just 2) (Just (toKey (10::Integer))) "monsterism" "The quality of being a monster"
--- web-executable/Products/ProductsAPI.hs
+++ web-executable/Products/ProductsAPI.hs
@@ -6,118 +6,117 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Products.ProductsAPI
-  ( ProductsAPI
-  , productsAPI
-  , productsServer
-  ) where
+( ProductsAPI
+, productsAPI
+, productsServer
+) where
 
-  import App (App (..))
-  import Control.Monad (mzero)
-  import Control.Monad.Except (runExceptT)
-  import Control.Monad.IO.Class (liftIO)
-  import Control.Monad.Reader (lift)
-  import Control.Monad.Trans.Either (left)
-  import Data.Aeson
-  import qualified Data.Text               as T
-  import qualified Data.ByteString.Lazy.Char8 as BS
-  import Models
-  import qualified Products.DomainTermsAPI as DT
-  import qualified Products.FeaturesAPI    as F
-  import qualified Products.Product        as P
-  import Servant
-  import qualified Servant.Docs            as SD
-  import ServantUtilities (Handler)
-  import qualified Products.UserRolesAPI   as UR
+import App (App (..))
+import Control.Monad (mzero)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (lift)
+import Control.Monad.Trans.Either (left)
+import Data.Aeson
+import qualified Data.Text               as T
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Models
+import qualified Products.DomainTermsAPI as DT
+import qualified Products.FeaturesAPI    as F
+import qualified Products.Product        as P
+import Servant
+import qualified Servant.Docs            as SD
+import qualified Products.UserRolesAPI   as UR
 
-  type ProductsAPI = "products" :> Get '[JSON] [APIProduct]
-                :<|> "products" :> ReqBody '[JSON] APIProduct :> Post '[JSON] APIProduct
-                :<|> "products" :> ProductIDCapture :> F.FeaturesAPI
-                :<|> "products" :> ProductIDCapture :> F.FeatureAPI
-                {- :<|> "products" :> ProductIDCapture :> DT.DomainTermsAPI -}
-                {- :<|> "products" :> ProductIDCapture :> DT.CreateDomainTermsAPI -}
-                {- :<|> "products" :> ProductIDCapture :> UR.UserRolesAPI -}
-                {- :<|> "products" :> ProductIDCapture :> UR.CreateUserRolesAPI -}
+type ProductsAPI = "products" :> Get '[JSON] [APIProduct]
+              :<|> "products" :> ReqBody '[JSON] APIProduct :> Post '[JSON] APIProduct
+              :<|> "products" :> ProductIDCapture :> F.FeaturesAPI
+              :<|> "products" :> ProductIDCapture :> F.FeatureAPI
+              :<|> "products" :> ProductIDCapture :> DT.DomainTermsAPI
+              :<|> "products" :> ProductIDCapture :> DT.CreateDomainTermsAPI
+              {- :<|> "products" :> ProductIDCapture :> UR.UserRolesAPI -}
+              {- :<|> "products" :> ProductIDCapture :> UR.CreateUserRolesAPI -}
 
-  type ProductIDCapture = Capture "id" P.ProductID
+type ProductIDCapture = Capture "id" P.ProductID
 
-  data APIProduct = APIProduct { productID :: Maybe P.ProductID
-                               , name      :: T.Text
-                               , repoUrl   :: T.Text
-                               } deriving (Show)
+data APIProduct = APIProduct { productID :: Maybe P.ProductID
+                             , name      :: T.Text
+                             , repoUrl   :: T.Text
+                             } deriving (Show)
 
-  instance ToJSON APIProduct where
-    toJSON (APIProduct prodID prodName prodRepoUrl) =
-      object [ "id"      .= prodID
-             , "name"    .= prodName
-             , "repoUrl" .= prodRepoUrl
-             ]
+instance ToJSON APIProduct where
+  toJSON (APIProduct prodID prodName prodRepoUrl) =
+    object [ "id"      .= prodID
+           , "name"    .= prodName
+           , "repoUrl" .= prodRepoUrl
+           ]
 
-  instance FromJSON APIProduct where
-    parseJSON (Object v) = APIProduct <$>
-                          v .:? "id" <*>
-                          v .: "name" <*>
-                          v .: "repoUrl"
-    parseJSON _          = mzero
+instance FromJSON APIProduct where
+  parseJSON (Object v) = APIProduct <$>
+                        v .:? "id" <*>
+                        v .: "name" <*>
+                        v .: "repoUrl"
+  parseJSON _          = mzero
 
-  productsServer :: ServerT ProductsAPI App
-  productsServer = products
-              :<|> createProduct
-              :<|> F.productsFeatures
-              :<|> F.productsFeature
-              {- :<|> DT.productsDomainTerms -}
-              {- :<|> DT.createDomainTerm -}
-              {- :<|> UR.productsUserRoles -}
-              {- :<|> UR.createUserRole -}
+productsServer :: ServerT ProductsAPI App
+productsServer = products
+            :<|> createProduct
+            :<|> F.productsFeatures
+            :<|> F.productsFeature
+            :<|> DT.productsDomainTerms
+            :<|> DT.createDomainTerm
+            {- :<|> UR.productsUserRoles -}
+            {- :<|> UR.createUserRole -}
 
-  productsAPI :: Proxy ProductsAPI
-  productsAPI = Proxy
+productsAPI :: Proxy ProductsAPI
+productsAPI = Proxy
 
-  createProduct :: APIProduct -> App APIProduct
-  createProduct (APIProduct _ prodName prodRepoUrl) = do
-    let newProduct = P.Product prodName prodRepoUrl
-    result <- liftIO $ runExceptT $ P.createProduct newProduct
-    case result of
-      Left err ->
-        -- In the case where the repo cannot be retrieved,
-        -- It's probably a good idea to rollback the Product creation here.
-        lift $ left $ err503 { errBody = BS.pack err }
-      Right prodID ->
-        return $ APIProduct { productID = Just prodID
-                            , name      = prodName
-                            , repoUrl   = prodRepoUrl
-                            }
+createProduct :: APIProduct -> App APIProduct
+createProduct (APIProduct _ prodName prodRepoUrl) = do
+  let newProduct = P.Product prodName prodRepoUrl
+  result <- liftIO $ runExceptT $ P.createProduct newProduct
+  case result of
+    Left err ->
+      -- In the case where the repo cannot be retrieved,
+      -- It's probably a good idea to rollback the Product creation here.
+      lift $ left $ err503 { errBody = BS.pack err }
+    Right prodID ->
+      return $ APIProduct { productID = Just prodID
+                          , name      = prodName
+                          , repoUrl   = prodRepoUrl
+                          }
 
-  products :: App [APIProduct]
-  products = do
-    prods <- liftIO P.findProducts
-    return $ map toProduct prods
-      where
-        toProduct dbProduct = do
-          let dbProd   = P.toProduct dbProduct
-          let dbProdID = P.toProductID dbProduct
-          APIProduct { productID = Just dbProdID
-                     , name      = productName dbProd
-                     , repoUrl   = productRepoUrl dbProd }
+products :: App [APIProduct]
+products = do
+  prods <- liftIO P.findProducts
+  return $ map toProduct prods
+    where
+      toProduct dbProduct = do
+        let dbProd   = P.toProduct dbProduct
+        let dbProdID = P.toProductID dbProduct
+        APIProduct { productID = Just dbProdID
+                   , name      = productName dbProd
+                   , repoUrl   = productRepoUrl dbProd }
 
-  -- API Documentation Instance Definitions --
+-- API Documentation Instance Definitions --
 
-  instance SD.ToSample [APIProduct] [APIProduct] where
-    toSample _ = Just $ [ sampleMonsterProduct, sampleCreatureProduct ]
+instance SD.ToSample [APIProduct] [APIProduct] where
+  toSample _ = Just $ [ sampleMonsterProduct, sampleCreatureProduct ]
 
-  instance SD.ToSample APIProduct APIProduct where
-    toSample _ = Just sampleCreatureProduct
+instance SD.ToSample APIProduct APIProduct where
+  toSample _ = Just sampleCreatureProduct
 
-  instance SD.ToCapture (Capture "id" P.ProductID) where
-    toCapture _ = SD.DocCapture "id" "Product id"
+instance SD.ToCapture (Capture "id" P.ProductID) where
+  toCapture _ = SD.DocCapture "id" "Product id"
 
-  sampleMonsterProduct :: APIProduct
-  sampleMonsterProduct = APIProduct { productID = Just 1
-                                    , name      = "monsters"
-                                    , repoUrl   = "http://monsters.com/repo.git"
-                                    }
+sampleMonsterProduct :: APIProduct
+sampleMonsterProduct = APIProduct { productID = Just 1
+                                  , name      = "monsters"
+                                  , repoUrl   = "http://monsters.com/repo.git"
+                                  }
 
-  sampleCreatureProduct :: APIProduct
-  sampleCreatureProduct = APIProduct { productID = Just 2
-                                     , name      = "creatures"
-                                     , repoUrl   = "ssh://creatures.com/repo.git"
-                                     }
+sampleCreatureProduct :: APIProduct
+sampleCreatureProduct = APIProduct { productID = Just 2
+                                   , name      = "creatures"
+                                   , repoUrl   = "ssh://creatures.com/repo.git"
+                                   }
