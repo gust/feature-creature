aae5e9c950c1c90f77a77ed306b5cd67754fce29
Todd Mohney <toddmohney@gmail.com>
2015-12-30 11:24:17 -0500

    Fix (properly, this time) CORS-related OPTIONS request errors
    [gust/feature-creature#60]

--- web-executable/Main.hs
+++ web-executable/Main.hs
@@ -7,18 +7,16 @@ import App
 import AppConfig (AppConfig, getAppConfig)
 import Control.Monad.Reader       (runReaderT)
 import Control.Monad.Trans.Either (EitherT)
+import Data.Monoid ((<>))
 import Documentation as Docs
 import Products.ProductsAPI (ProductsAPI, productsServer)
 import Network.Wai as Wai
 import Network.Wai.Handler.Warp as Warp
-import Network.Wai.Middleware.AddHeaders
 import Network.Wai.Middleware.Cors
 import Servant
 
-type FeatureCreatureAPI = ProductsAPI :<|> Docs.DocumentationAPI
-
-{- type FeatureCreatureAPI = ProductsAPI :<|> Raw -}
-{- type FeatureCreatureAPI = ProductsAPI -}
+type FeatureCreatureAPI = ProductsAPI
+                     :<|> Docs.DocumentationAPI
 
 main :: IO ()
 main = do
@@ -40,19 +38,13 @@ server = productsServer
 api :: Proxy FeatureCreatureAPI
 api = Proxy
 
-addResponseHeaders :: Middleware
-addResponseHeaders =
-  addHeaders [ ("Access-Control-Allow-Origin", "*")
-             , ("Access-Control-Allow-Methods", "*")
-             , ("Access-Control-Request-Method", "*")
-             , ("Access-Control-Allow-Headers", "Content-Type")
-             , ("Origin", "*")
-             ]
-
-{- app :: Application -}
-{- app = addResponseHeaders $ serve api server -}
-
 app :: AppConfig -> Wai.Application
 app cfg =
-  simpleCors
+  cors (const $ Just corsPolicy)
   $ serve api (readerServer cfg)
+
+corsPolicy :: CorsResourcePolicy
+corsPolicy =
+  simpleCorsResourcePolicy { corsMethods = simpleMethods <> ["DELETE", "PUT", "OPTIONS"]
+                           , corsRequestHeaders = ["Content-Type"]
+                           }
