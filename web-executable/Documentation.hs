0d91fcadc5e2031e87026ec69ad68f3a2ca3293e
Todd Mohney <toddmohney@gmail.com>
2015-12-30 10:08:39 -0500

    Assign API Documentation to a dedicated route
    [gust/feature-creature#60]

--- web-executable/Documentation.hs
+++ web-executable/Documentation.hs
@@ -1,26 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
-module Documentation where
+module Documentation
+( DocumentationAPI
+, documentationServer
+) where
 
-import Control.Monad.Reader    (lift)
-import Data.ByteString.Lazy    (ByteString)
-import Data.Text.Lazy          (pack)
-import Data.Text.Lazy.Encoding (encodeUtf8)
-import Network.HTTP.Types      (ok200)
-import Network.Wai             (responseLBS)
+import App
+import qualified Data.ByteString.Lazy   as LBS
+import Data.Text               (Text, pack)
+import Data.Text.Encoding      (encodeUtf8)
+import Network.HTTP.Media      ((//), (/:))
 import Products.ProductsAPI    (productsAPI)
+import Servant
 import qualified Servant.Docs as SD
 
--- not sure of the type here. snippet taken from: http://haskell-servant.github.io/tutorial/docs.html
-documentationServer _ respond =
-  respond $ responseLBS ok200 [plain] docsBS
-  where
-    plain       = ("Content-Type", "text/plain")
+type DocumentationAPI = "docs" :> Get '[Markdown] Text
 
-docsBS :: ByteString
-docsBS = encodeUtf8
-       . pack
-       . SD.markdown
-       $ SD.docsWithIntros [intro] productsAPI
-  where
-    intro = SD.DocIntro "feature-creature" ["![](http://www.homecinemachoice.com/sites/18/images/article_images_month/2012-07/universal%20monsters%20news%2001.jpg)", "Welcome to our API", "Feel free to dig around"]
+data Markdown
+
+instance Servant.Accept Markdown where
+    contentType _ = "text" // "markdown" /: ("charset", "utf-8")
+
+instance Servant.MimeRender Markdown Text where
+    mimeRender _ text = LBS.fromStrict (encodeUtf8 text)
+
+documentationServer :: App Text
+documentationServer = return (pack markdown)
+
+markdown :: String
+markdown = SD.markdown (SD.docsWithIntros [intro] productsAPI)
+
+intro :: SD.DocIntro
+intro = SD.DocIntro "feature-creature" ["![](http://www.homecinemachoice.com/sites/18/images/article_images_month/2012-07/universal%20monsters%20news%2001.jpg)", "Welcome to our API", "Feel free to dig around"]
--- web-executable/Main.hs
+++ web-executable/Main.hs
@@ -9,18 +9,21 @@ import Control.Monad.Reader       (runReaderT)
 import Control.Monad.Trans.Either (EitherT)
 import Documentation as Docs
 import Products.ProductsAPI (ProductsAPI, productsServer)
-import Network.Wai
-import Network.Wai.Handler.Warp
+import Network.Wai as Wai
+import Network.Wai.Handler.Warp as Warp
 import Network.Wai.Middleware.AddHeaders
+import Network.Wai.Middleware.Cors
 import Servant
 
+type FeatureCreatureAPI = ProductsAPI :<|> Docs.DocumentationAPI
+
 {- type FeatureCreatureAPI = ProductsAPI :<|> Raw -}
-type FeatureCreatureAPI = ProductsAPI
+{- type FeatureCreatureAPI = ProductsAPI -}
 
 main :: IO ()
 main = do
   appConfig <- getAppConfig
-  run 8081 (app appConfig)
+  Warp.run 8081 (app appConfig)
 
 readerToEither :: AppConfig -> App :~> EitherT ServantErr IO
 readerToEither cfg =
@@ -31,9 +34,8 @@ readerServer cfg =
   enter (readerToEither cfg) server
 
 server :: ServerT FeatureCreatureAPI App
-server =
-  productsServer
-  {- :<|> Docs.documentationServer -}
+server = productsServer
+    :<|> Docs.documentationServer
 
 api :: Proxy FeatureCreatureAPI
 api = Proxy
@@ -41,6 +43,7 @@ api = Proxy
 addResponseHeaders :: Middleware
 addResponseHeaders =
   addHeaders [ ("Access-Control-Allow-Origin", "*")
+             , ("Access-Control-Allow-Methods", "*")
              , ("Access-Control-Request-Method", "*")
              , ("Access-Control-Allow-Headers", "Content-Type")
              , ("Origin", "*")
@@ -49,7 +52,7 @@ addResponseHeaders =
 {- app :: Application -}
 {- app = addResponseHeaders $ serve api server -}
 
-app :: AppConfig -> Application
+app :: AppConfig -> Wai.Application
 app cfg =
-  addResponseHeaders
+  simpleCors
   $ serve api (readerServer cfg)
