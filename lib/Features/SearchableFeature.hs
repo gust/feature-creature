3762b04430b66b1edb95fe42f451773d367870de
Todd Mohney <toddmohney@gmail.com>
2016-01-09 15:15:53 -0500

    Improve Features.Search interface to hide internal Bloodhound implementation
    [gust/feature-creature#56]
    [gust/feature-creature#55]

--- lib/Features/SearchableFeature.hs
+++ lib/Features/SearchableFeature.hs
@@ -1,64 +1,66 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Features.SearchableFeature where
-  import Database.Bloodhound
-  import Database.Bloodhound.Types as BHTypes
-  import Data.Aeson
-  import Data.Text (Text)
-  import qualified Data.Vector as V
-  import GHC.Generics (Generic)
-  import Network.HTTP.Client
+module Features.SearchableFeature
+( SearchableFeature (..)
+, indexFeatures
+, searchFeatures
+) where
+import Database.Bloodhound
+import Data.Aeson
+import Data.Maybe (mapMaybe)
+import Data.Text (Text, pack)
+import qualified Data.Vector as V
+import GHC.Generics (Generic)
+import Network.HTTP.Client
 
-  data SearchableFeature =
-    SearchableFeature { featurePath :: Text
-                      , featureText :: Text
-                      } deriving (Show, Generic)
+data SearchableFeature =
+  SearchableFeature { featurePath :: Text
+                    , featureText :: Text
+                    } deriving (Show, Generic)
 
-  instance ToJSON   SearchableFeature
-  instance FromJSON SearchableFeature
+instance ToJSON   SearchableFeature
+instance FromJSON SearchableFeature
 
-  sampleSearchableFeature :: SearchableFeature
-  sampleSearchableFeature =
-    SearchableFeature { featurePath = "/some/cool/path"
-                      , featureText = "Given blah When blah Then more blah"
-                      }
+indexFeatures :: [SearchableFeature] -> IO ()
+indexFeatures searchableFeatures =
+  let indicies = map createBulkIndex searchableFeatures
+      stream = V.fromList indicies :: V.Vector BulkOperation
+  in
+    -- we're outputting something unhelpful here to hide the
+    -- internal BH types from the user. we get a Response back
+    -- which we can log in the future
+    withBH' (bulk stream) >>= putStrLn . show
+  where
+    createBulkIndex f =
+      BulkIndex
+        featureCreatureIndex
+        featureMapping
+        (DocId (featurePath f))
+        (toJSON f)
 
-  indexFeatures :: [SearchableFeature] -> IO BHTypes.Reply
-  indexFeatures searchableFeatures =
-    let indicies = map createBulkIndex searchableFeatures
-        stream = V.fromList indicies :: V.Vector BulkOperation
-    in
-      withBH' $ bulk stream
-    where
-      createBulkIndex f =
-        BulkIndex
-          testIndex
-          testMapping
-          (DocId (featurePath f))
-          (toJSON f)
+-- search is not constrained to a mapping
+searchFeatures :: Text -> IO [SearchableFeature]
+searchFeatures queryStr = do
+  reply <- withBH' $ searchByIndex featureCreatureIndex search
+  let results = eitherDecode (responseBody reply) :: Either String (SearchResult SearchableFeature)
+  case fmap (hits . searchHits) results of
+    Left str   -> return [ (SearchableFeature (pack str) (pack str)) ]
+    Right searchResultHits -> return $ mapMaybe hitSource searchResultHits
+  where
+    search = mkSearch (Just query) (Just searchFilter)
+    searchFilter = IdentityFilter <&&> IdentityFilter
+    query = QueryMatchQuery $ mkMatchQuery (FieldName "featureText") (QueryString queryStr)
 
-  indexFeature :: SearchableFeature -> IO BHTypes.Reply
-  indexFeature searchableFeature =
-    withBH' $ indexDocument testIndex testMapping defaultIndexDocumentSettings searchableFeature (DocId (featurePath searchableFeature))
+withBH' :: BH IO a -> IO a
+withBH' = withBH defaultManagerSettings testServer
 
-  searchFeatures :: Text -> IO BHTypes.Reply
-  searchFeatures queryStr =
-    withBH' $ searchByIndex testIndex search
-    where
-      search = mkSearch (Just query) (Just filter)
-      filter = IdentityFilter <&&> IdentityFilter
-      {- filter = IdentityFilter <||> IdentityFilter -}
-      {- query = TermQuery (Term "feature" queryStr) Nothing -}
-      query = QueryMatchQuery $ mkMatchQuery (FieldName "featureText") (QueryString queryStr)
+featureMapping :: MappingName
+featureMapping = MappingName "feature"
 
-  withBH' = withBH defaultManagerSettings testServer
+featureCreatureIndex :: IndexName
+featureCreatureIndex = IndexName "feature-creature"
 
-  testMapping :: MappingName
-  testMapping = MappingName "feature"
-
-  testIndex :: IndexName
-  testIndex = IndexName "feature-creature"
-
-  testServer :: Server
-  testServer = Server "http://localhost:9200"
+-- we need to add this to Config
+testServer :: Server
+testServer = Server "http://localhost:9200"
