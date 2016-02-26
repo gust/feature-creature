f844ed99b65543ba941fb987594225a09f14c4ed
Todd Mohney <toddmohney@gmail.com>
2016-02-26 16:29:03 -0500

    Find and print all Products
    [gust/feature-creature#80]

--- repo-refresher/Main.hs
+++ repo-refresher/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import App
 import AppConfig as Cfg
 import Control.Monad.Reader
+import qualified Database.Persist.Postgresql as DB
 import Products.Product as P
 
 main :: IO ()
@@ -11,7 +12,19 @@ main = do
   runReaderT refreshRepos appConfig
 
 refreshRepos :: App ()
-refreshRepos = do
-  dbCfg    <- reader getDBConfig
-  products <- liftIO $ P.findProducts dbCfg
-  liftIO $ mapM_ (putStrLn . show . productName . toProduct) products
+refreshRepos =
+  getProducts
+    >>= (\products -> liftIO $ printProducts products)
+
+getProducts :: App [Product]
+getProducts = do
+  getProductEntities
+    >>= (\entities -> return $ map P.toProduct entities)
+
+getProductEntities :: App [DB.Entity Product]
+getProductEntities =
+  reader getDBConfig
+    >>= (\cfg -> liftIO $ P.findProducts cfg)
+
+printProducts :: [Product] -> IO ()
+printProducts = mapM_ (putStrLn . show . productName)
