0a15a8ba76ed80a45a7ddc1f59dc051020778c1e
Todd Mohney <toddmohney@gmail.com>
2016-02-26 18:13:59 -0500

    Refactor Git module
    [gust/feature-creature#80]

--- lib/Git.hs
+++ lib/Git.hs
@@ -1,5 +1,6 @@
 module Git
 ( clone
+, diff
 , fetch
 , pull
 ) where
@@ -7,25 +8,39 @@ module Git
 import CommonCreatures (WithErr)
 import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class (liftIO)
+import Data.Monoid ((<>))
 import qualified Data.Text as T
 import System.Exit (ExitCode(ExitFailure, ExitSuccess))
 import System.Process (readProcessWithExitCode)
 
 clone :: FilePath -> T.Text -> WithErr String
 clone path url = do
-  result <- liftIO $ readProcessWithExitCode "git" ["clone", (T.unpack url), path] ""
+  result <- liftIO $ gitCommand ["clone", (T.unpack url), path]
   parseResult result
 
 pull :: FilePath -> WithErr String
 pull path = do
-  result <- liftIO $ readProcessWithExitCode "git" ["-C", path, "pull"] ""
+  result <- liftIO $ gitRepoCommand path ["pull"]
   parseResult result
 
 fetch :: FilePath -> WithErr String
 fetch path = do
-  result <- liftIO $ readProcessWithExitCode "git" ["-C", path, "fetch"] ""
+  result <- liftIO $ gitRepoCommand path ["fetch"]
+  parseResult result
+
+diff :: FilePath -> WithErr String
+diff path = do
+  result <- liftIO $ gitRepoCommand path ["diff", "--name-status"]
   parseResult result
 
 parseResult :: (ExitCode, String, String) -> WithErr String
 parseResult (ExitFailure _, stdout, stderr) = throwError $ stderr ++ stdout
 parseResult (ExitSuccess, stdout, stderr)   = return $ stderr ++ stdout
+
+gitRepoCommand :: FilePath -> [String] -> IO (ExitCode, String, String)
+gitRepoCommand path args = gitCommand $ defaults <> args
+  where
+    defaults = ["-C", path]
+
+gitCommand :: [String] -> IO (ExitCode, String, String)
+gitCommand args = readProcessWithExitCode "git" args ""
