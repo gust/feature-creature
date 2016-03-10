c21f841aa4fbafbb7c4f16988a8ab3a77b9381f8
Todd Mohney <toddmohney@gmail.com>
2016-03-09 21:34:47 -0500

    Magic refactoring of Features module

--- search-service/Features.hs
+++ search-service/Features.hs
@@ -4,20 +4,19 @@ module Features
 
 import CommonCreatures (WithErr)
 import AppConfig (ElasticSearchConfig, GitConfig)
-import Control.Monad.Except (runExceptT, throwError, liftIO)
+import Control.Monad.IO.Class (liftIO)
 import Features.Feature (FeatureFile, findFeatureFiles)
 import qualified Indexer
 import Products.CodeRepository (CodeRepository(..), codeRepositoryDir)
 import Products.Product (ProductID)
 
 indexFeatures :: CodeRepository -> GitConfig -> ElasticSearchConfig -> WithErr ()
-indexFeatures (CodeRepository productID) gitConfig esConfig = do
-  (liftIO $ runExceptT $ findFeatureFiles (codeRepositoryDir productID gitConfig))
+indexFeatures (CodeRepository productID) gitConfig esConfig =
+  (findFeatureFiles (codeRepositoryDir productID gitConfig))
     >>= (\featureFiles -> indexFeatures' featureFiles productID gitConfig esConfig)
 
-indexFeatures' :: Either String [FeatureFile] -> ProductID -> GitConfig -> ElasticSearchConfig -> WithErr ()
-indexFeatures' (Left err) _ _ _   = throwError err
-indexFeatures' (Right features) productID gitConfig esConfig =
+indexFeatures' :: [FeatureFile] -> ProductID -> GitConfig -> ElasticSearchConfig -> WithErr ()
+indexFeatures' features productID gitConfig esConfig =
   (liftIO $ Indexer.indexFeatures features productID gitConfig esConfig)
     >> return ()
 
