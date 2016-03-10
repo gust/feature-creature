d24e3c093b11d9ad3d9280bb07fb1c72d5ba64d8
Todd Mohney <toddmohney@gmail.com>
2016-03-09 21:18:46 -0500

    Refactor search-service Features.hs

--- search-service/Features.hs
+++ search-service/Features.hs
@@ -2,19 +2,22 @@ module Features
 ( indexFeatures
 ) where
 
-import           CommonCreatures (WithErr)
-import           AppConfig (ElasticSearchConfig, GitConfig)
-import           Control.Monad.Except (runExceptT, throwError, liftIO)
-import           Features.Feature (findFeatureFiles)
+import CommonCreatures (WithErr)
+import AppConfig (ElasticSearchConfig, GitConfig)
+import Control.Monad.Except (runExceptT, throwError, liftIO)
+import Features.Feature (FeatureFile, findFeatureFiles)
 import qualified Indexer
-import           Products.CodeRepository (CodeRepository(..), codeRepositoryDir)
+import Products.CodeRepository (CodeRepository(..), codeRepositoryDir)
+import Products.Product (ProductID)
 
 indexFeatures :: CodeRepository -> GitConfig -> ElasticSearchConfig -> WithErr ()
 indexFeatures (CodeRepository productID) gitConfig esConfig = do
-  let featureFileBasePath = codeRepositoryDir productID gitConfig
-  featureFiles <- liftIO $ runExceptT $ findFeatureFiles featureFileBasePath
-  case featureFiles of
-    Left errorStr ->
-      throwError errorStr
-    Right features -> do
-      (liftIO $ Indexer.indexFeatures features productID gitConfig esConfig) >> return ()
+  (liftIO $ runExceptT $ findFeatureFiles (codeRepositoryDir productID gitConfig))
+    >>= (\featureFiles -> indexFeatures' featureFiles productID gitConfig esConfig)
+
+indexFeatures' :: Either String [FeatureFile] -> ProductID -> GitConfig -> ElasticSearchConfig -> WithErr ()
+indexFeatures' (Left err) _ _ _   = throwError err
+indexFeatures' (Right features) productID gitConfig esConfig =
+  (liftIO $ Indexer.indexFeatures features productID gitConfig esConfig)
+    >> return ()
+
