cbe51fce3ed36ee05dc01dd55e0cd8db1ca8f6d7
Todd Mohney <toddmohney@gmail.com>
2016-03-09 22:28:40 -0500

    Middleman refactor of search-service Features

--- search-service/Main.hs
+++ search-service/Main.hs
@@ -2,17 +2,19 @@
 
 module Main where
 
-import           App
-import           AppConfig (AppConfig(..), readConfig)
-import           Async.Job as Job
-import           CommonCreatures (WithErr)
-import           Control.Concurrent (threadDelay)
-import           Control.Monad.Except (runExceptT, throwError)
-import           Control.Monad.Reader
+import App
+import AppConfig (AppConfig(..), readConfig)
+import Async.Job as Job
+import CommonCreatures (WithErr)
+import Control.Concurrent (threadDelay)
+import Control.Monad.Except (runExceptT, throwError)
+import Control.Monad.Reader
 import qualified Data.Text as Text
-import qualified Features
-import           Products.CodeRepository (CodeRepository(..))
-import           SQS (getSQSMessages, deleteSQSMessage)
+import Features.Feature (FeatureFile, findFeatureFiles)
+import qualified Indexer
+import Products.CodeRepository (CodeRepository(..), codeRepositoryDir)
+import Products.Product (ProductID)
+import SQS (getSQSMessages, deleteSQSMessage)
 
 main :: IO ()
 main = readConfig >>= runReaderT processJobs
@@ -28,24 +30,35 @@ processJobs =
 processEnqueuedJob :: Either String (EnqueuedJob CodeRepository) -> App ()
 processEnqueuedJob (Left err) = liftIO $ putStrLn err
 processEnqueuedJob (Right (EnqueuedJob job deliveryReceipt)) = do
-  processJob job
+  processJob (Job.getPayload job) job
     >>= (liftIO . runExceptT)
     >>= (\result -> processJobResult result deliveryReceipt)
 
-processJob :: Job CodeRepository -> App (WithErr ())
-processJob job = do
-  case Job.getPayload job of
-    CodeRepository _ ->
-      ask >>= (\cfg ->
-        let gitConfig = getGitConfig cfg
-            esConfig  = getElasticSearchConfig cfg
-        in return $ Features.indexFeatures (Job.getPayload job) gitConfig esConfig)
-    _ ->
-      return $ throwError $ "Unprocessable job type: " ++ (Text.unpack $ Job.getJobType job)
+processJob :: CodeRepository -> Job a -> App (WithErr ())
+processJob (CodeRepository productID) _ =
+  (featureFiles productID)
+    >>= (liftIO . runExceptT)
+    >>= (\files -> indexFeatures files productID)
+processJob _ job =
+  return
+    $ throwError
+    $ "Unprocessable job type: " ++ (Text.unpack $ Job.getJobType job)
 
 processJobResult :: Either String () -> Text.Text -> App ()
 processJobResult (Left errStr) _ = liftIO $ putStrLn errStr
 processJobResult (Right _) deliveryReceipt =
   reader getAWSConfig
     >>= (liftIO . (deleteSQSMessage deliveryReceipt))
+
+featureFiles :: ProductID -> App (WithErr [FeatureFile])
+featureFiles productID =
+  (reader getGitConfig)
+    >>= (\gitConfig -> return $ findFeatureFiles (codeRepositoryDir productID gitConfig))
+
+indexFeatures :: Either String [FeatureFile] -> ProductID -> App (WithErr ())
+indexFeatures (Left err) _ = return $ throwError err
+indexFeatures (Right features) productID =
+  ask
+    >>= (\cfg -> (liftIO $ Indexer.indexFeatures features productID (getGitConfig cfg) (getElasticSearchConfig cfg)))
+    >> return (return ())
 
