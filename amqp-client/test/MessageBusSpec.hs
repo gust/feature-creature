8aafe8a504d69434e912493a2781f2bba3bf40f9
Todd Mohney <toddmohney@gmail.com>
2016-08-22 08:56:57 -0400

    Test production/consumption of AMQP messages

--- amqp-client/test/MessageBusSpec.hs
+++ amqp-client/test/MessageBusSpec.hs
@@ -1,6 +1,11 @@
 module MessageBusSpec where
 
+import Control.Concurrent (threadDelay)
+import Data.Aeson as Aeson
+import Data.Text (Text)
 import Data.Word (Word32)
+import GHC.Generics (Generic)
+import qualified Network.AMQP as AMQP
 import Network.AMQP.Config (RabbitMQConfig (..))
 import Network.AMQP.MessageBus
   ( Exchange (..)
@@ -9,6 +14,9 @@ import Network.AMQP.MessageBus
   , Queue (..)
   , QueueName (..)
   , QueueStatus
+  , Message (..)
+  , MessageHandler (..)
+  , WithConn (..)
   , createExchange
   , deleteExchange
   , createQueue
@@ -27,37 +35,63 @@ spec :: Spec
 spec = do
   describe "Enqueuing and dequeuing messages" $
     it "writes and reads messages from the queue" $ do
-      createTestExchange
-        >> createTestQueue >>= putStrLn . show
-        >> subscribeToTopic (QueueName "test-queue") (TopicName "*.test.created")
-        -- TODO: produce a message
-        -- TODO: consume that message
-        >> deleteTestQueue >>= putStrLn . show
-        >> deleteTestExchange
+      createSubscribeProduce
+      threadDelay 1000 -- give the message a little time to persist
+      consumeDelete
+
+      -- Message processing happens in a different thread.
+      -- For now we can look at stdout as the test runs to visually verify
+      -- the correct behavior
+      --
+      -- How could we make a meaningful assertion here?
+      -- Perhaps the message handler could write to a file, then we could
+      -- read and assert against the contents. There's probably a much
+      -- better way to test this.
       True `shouldBe` True
 
-subscribeToTopic :: QueueName -> TopicName -> IO ()
-subscribeToTopic queueName topicName =
-  withConn testConfig (subscribe queueName topicName)
+createSubscribeProduce :: IO ()
+createSubscribeProduce = withConn testConfig $ do
+  createTestExchange
+    >> createTestQueue
+    >> subscribeToTopic testQueue testTopic
+    >> produceMessage testMessage
 
-createTestQueue :: IO QueueStatus
+consumeDelete :: IO ()
+consumeDelete = withConn testConfig $ do
+  consumeMessage testHandler
+    >> deleteTestQueue
+    >> deleteTestExchange
+
+
+consumeMessage :: MessageHandler -> WithConn ()
+consumeMessage handler =
+  getTopicMessages testQueue handler
+
+produceMessage :: (ToJSON a) => a -> WithConn ()
+produceMessage msg =
+  produceTopicMessage testTopic (Message msg)
+
+subscribeToTopic :: QueueName -> TopicName -> WithConn ()
+subscribeToTopic queueName topicName =
+  subscribe queueName topicName
+
+createTestQueue :: WithConn QueueStatus
 createTestQueue =
   let queue = Queue "test-queue" False True
-  in withConn testConfig (createQueue queue)
+  in createQueue queue
 
-deleteTestQueue :: IO Word32
+deleteTestQueue :: WithConn Word32
 deleteTestQueue =
   let queueName = QueueName "test-queue"
-  in withConn testConfig (deleteQueue queueName)
+  in deleteQueue queueName
 
-createTestExchange :: IO ()
-createTestExchange =
-  withConn testConfig (createExchange testExchange)
+createTestExchange :: WithConn ()
+createTestExchange = createExchange testExchange
 
-deleteTestExchange :: IO ()
+deleteTestExchange :: WithConn ()
 deleteTestExchange =
   let exchangeName = ExchangeName . getExchangeName $ testExchange
-  in withConn testConfig (deleteExchange exchangeName)
+  in deleteExchange exchangeName
 
 testExchange :: Exchange
 testExchange = Exchange (getExchangeNameConfig testConfig) "topic" True
@@ -70,3 +104,24 @@ testConfig = RabbitMQConfig
   , getPassConfig = "guest"
   , getExchangeNameConfig = "test-exchange"
   }
+
+data TestMessage = TestMessage Int Text
+  deriving (Show, Eq, Generic)
+
+instance ToJSON TestMessage
+instance FromJSON TestMessage
+
+testMessage :: TestMessage
+testMessage = TestMessage 1 "burgertime"
+
+testTopic :: TopicName
+testTopic = TopicName "*.test.created"
+
+testQueue :: QueueName
+testQueue = QueueName "test-queue"
+
+testHandler :: MessageHandler
+testHandler = MessageHandler handlerFn
+  where
+    handlerFn :: (AMQP.Message, AMQP.Envelope) -> IO ()
+    handlerFn (msg, _) = putStrLn (show msg)
