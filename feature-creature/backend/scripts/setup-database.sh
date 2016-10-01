54f9ec1c18dbead5de7a29031bb821ab0b6edb64
Todd Mohney <toddmohney@gmail.com>
2016-10-01 17:18:50 -0400

    Configure env to work with docker managed storage containers

--- feature-creature/backend/scripts/setup-database.sh
+++ feature-creature/backend/scripts/setup-database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FC_USERS_SERVICE_USER=fc_feature_creature
-FC_USERS_SERVICE_DB_NAME=fc_feature_creature
-FC_USERS_SERVICE_TEST_DB_NAME=fc_feature_creature_test
+FC_APP_USER=fc_feature_creature
+FC_APP_DB_NAME=fc_feature_creature
+FC_APP_TEST_DB_NAME=fc_feature_creature_test
 
-createuser -e $FC_USERS_SERVICE_USER
-createdb -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_DB_NAME
-createdb -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_TEST_DB_NAME
+docker run -it --rm postgres createuser -h 172.17.0.1 -U featurecreature -e $FC_APP_USER
+docker run -it --rm postgres createdb -h 172.17.0.1 -U featurecreature -e --owner=$FC_APP_USER $FC_APP_DB_NAME
+docker run -it --rm postgres createdb -h 172.17.0.1 -U featurecreature -e --owner=$FC_APP_USER $FC_APP_TEST_DB_NAME
--- users-service/scripts/setup-database.sh
+++ users-service/scripts/setup-database.sh
@@ -4,6 +4,6 @@ FC_USERS_SERVICE_USER=fc_users_service
 FC_USERS_SERVICE_DB_NAME=fc_users_service
 FC_USERS_SERVICE_TEST_DB_NAME=fc_users_service_test
 
-createuser -e $FC_USERS_SERVICE_USER
-createdb -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_DB_NAME
-createdb -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_TEST_DB_NAME
+docker run -it --rm postgres createuser -h 172.17.0.1 -U featurecreature -e $FC_USERS_SERVICE_USER
+docker run -it --rm postgres createdb -h 172.17.0.1 -U featurecreature -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_DB_NAME
+docker run -it --rm postgres createdb -h 172.17.0.1 -U featurecreature -e --owner=$FC_USERS_SERVICE_USER $FC_USERS_SERVICE_TEST_DB_NAME
